a05ed14be9c094206ffde66b6c13f709a02d60b0
Zach Estrella <zachgabriel27@gmail.com>
2026-08-09 22:49:42 +0800

    Update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,35 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Wedding Invitation",
-  description: "Sample Wedding Invitation of Josh Estrella & Claribel Acob",
+  description: "Wedding Invitation of Claribel & Josh",
+
+  openGraph: {
+    title: "Wedding Invitation",
+    description: "Wedding Invitation of Claribel & Josh",
+    images: [
+      {
+        url: "/assets/envelope-button.webp",
+        width: 1200,
+        height: 630,
+        alt: "Wedding Invitation",
+      },
+    ],
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    images: ["/assets/envelope-button.webp"],
+  },
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className="h-full antialiased"
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
