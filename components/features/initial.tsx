c198d5a50905fd262bef31d729a06c3cf9a78728
Zach Estrella <zachgabriel27@gmail.com>
2026-08-09 20:50:43 +0800

    Resized pictures and change from png to webp

--- components/features/initial.tsx
+++ components/features/initial.tsx
@@ -22,27 +22,25 @@ export default function Initial({setIsClicked}:InitialProps) {
             </div>
             
             <div className="relative mx-auto w-[60%] max-w-[500px]">
-                <Image src="/assets/envelope-before.png"
-                    width={500} height={500}
+                <Image src="/assets/envelope-before.webp"
+                    loading="eager"
+                    width={1900} height={1300}
                     className="relative z-30 h-auto w-full"
                     alt="Envelope"/>
                 <Image onClick={() => setIsClicked(true)}
-                    src="/assets/envelope-button.png"
-                    width={80} height={80}
+                    src="/assets/envelope-button.webp"
+                    width={500} height={500}
                     className="absolute left-1/2 top-1/2 z-40 h-auto w-[16%] -translate-x-1/2 -translate-y-1/2 cursor-pointer"
                     alt="Envelope button"/>
-                <Image src="/assets/flower-01.png"
-                    width={200} height={200}
+                <Image src="/assets/flower01.webp" width={500} height={500}
                     className="absolute bottom-0 left-[-35%] z-20 h-auto w-[60%] -rotate-30"
-                    alt="Flower 01"/>
-                <Image src="/assets/flower-02.png"
-                    width={200} height={200}
+                    alt="Flower"/>
+                <Image src="/assets/flower02.webp" width={500} height={500}
                     className="absolute top-[-18%] right-[-15%] z-50 h-auto w-[30%] -rotate-30"
-                    alt="Flower 02"/>
-                <Image src="/assets/flower-04.png"
-                    width={200} height={200}
+                    alt="Flower"/>
+                <Image src="/assets/flower03.webp" width={400} height={600}
                     className="absolute bottom-[-18%] right-[-15%] z-50 h-auto w-[30%] rotate-5"
-                    alt="Flower 04"/>
+                    alt="Flower"/>
             </div>
 
             <p className="text-3xl">(TAP THE ENVELOPE TO OPEN)</p>
--- components/features/rvsp.tsx
+++ components/features/rvsp.tsx
@@ -4,30 +4,30 @@ import Countdown from "./countdown"
 export default function Rvsp() {
     return (
         <section className="relative min-h-screen overflow-hidden px-12">
-            <Image src="/assets/bg.png" alt="bg" width={2000} height={4000} className="absolute inset-0 h-full w-full bg-local"/>
+            <Image src="/assets/bg.jpg" alt="bg" width={1920} height={1080} className="absolute inset-0 h-full w-full bg-local"/>
 
             <div className="relative z-10 min-h-screen
                             flex flex-col justify-center z-50 mx-auto gap-12 mt-24">
                 <div className="relative"> 
-                    <Image alt="Flower" src="/assets/flower-01.png" width={1400} height={1200} 
+                    <Image alt="Flower" src="/assets/flower01.webp" width={500} height={500} 
                             className="h-auto absolute z-40 rotate-28 
                             xl:w-[30%] md:w-[40%] w-[80%] 
                             xl:left-[10%] lg:-left-[2%] md:-left-[5%] -left-[25%] 
                             xl:-top-48 lg:-top-40 -top-28"/> 
-                    <Image alt="Flower" src="/assets/flower-03.png" width={1400} height={1200} 
+                    <Image alt="Flower" src="/assets/flower04.webp" width={270} height={600} 
                             className="h-auto absolute -translate-x-1/2 z-40 -rotate-16 
                             xl:w-[15%] md:w-[20%] w-[35%]
                             xl:left-[26%] lg:left-[21%] md:left-[17%] left-[15%]
                             xl:-top-24 lg:-top-24 md:-top-14 -top-12"/> 
-                    <Image alt="Envelope open" src="/assets/envelope-open.png" width={3800} height={2600} 
+                    <Image alt="Envelope open" src="/assets/envelope-open.webp" width={1900} height={1300} 
                             className="h-auto absolute -translate-x-1/2 -rotate-10 z-40 
                             xl:w-[30%] lg:w-[40%] md:w-[50%] w-[80%] 
                             left-[36%] top-2"/> 
-                    <Image alt="Envelope button" src="/assets/envelope-button.png" width={1200} height={1200} 
+                    <Image alt="Envelope button" src="/assets/envelope-button.webp" width={500} height={500} 
                             className="h-auto absolute -translate-x-1/2 -rotate-10 z-40 
                             lg:w-[10%] md:w-[15%] w-[20%] 
                             left-[37%] md:top-28 top-16"/> 
-                    <Image alt="Couple pic" src="/assets/couple-pic.png" width={1500} height={1800} 
+                    <Image alt="Couple pic" src="/assets/couple-pic.webp" width={1200} height={1500} 
                             className="h-auto absolute -translate-x-1/2 rotate-8 z-30
                             xl:w-[20%] lg:w-[30%] md:w-[40%] w-[60%] 
                             md:left-[38%] left-[42%]
@@ -35,12 +35,12 @@ export default function Rvsp() {
                 </div>
                 <div className="flex items-end md:gap-8 gap-0 z-50">
                     <div className="w-1/2 flex justify-end">
-                        <Image alt="RVSP image" src="/assets/click.png" width={2200} height={1700}
+                        <Image alt="RSVP" src="/assets/rsvp.webp" width={700} height={500}
                             className="h-auto -rotate-15 
                                     xl:w-[40%] lg:w-[60%] md:w-[80%] w-full"/>
                     </div>
                     <div className="w-1/2 flex justify-start mb-12">
-                        <Image alt="RVSP image" src="/assets/date.png" width={3400} height={4900}
+                        <Image alt="Details" src="/assets/details.webp" width={1200} height={1800}
                             className="h-auto rotate-10
                                     xl:w-[40%] lg:w-[60%] md:w-[80%] w-full"/>
                     </div>
