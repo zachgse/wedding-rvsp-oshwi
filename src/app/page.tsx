fedc31054861fd17c261a551897ceb27d5d9f43f
Zach Estrella <zachgabriel27@gmail.com>
2026-08-10 17:08:00 +0800

    Add bg song

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,34 @@
 "use client"
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Initial from "../../components/features/initial";
 import Rvsp from "../../components/features/rvsp";
 
 export default function Home() {
+  const audioRef = useRef<HTMLAudioElement>(null);
   const [isClicked,setIsClicked] = React.useState<boolean>(false);
+
+   useEffect(() => {
+    const startMusic = () => {
+      audioRef.current?.play().catch(() => {});
+
+      window.removeEventListener("click", startMusic);
+      window.removeEventListener("touchstart", startMusic);
+    };
+
+    window.addEventListener("click", startMusic);
+    window.addEventListener("touchstart", startMusic);
+
+    return () => {
+      window.removeEventListener("click", startMusic);
+      window.removeEventListener("touchstart", startMusic);
+    };
+  }, []);
 
   return (
     <div>
+      <audio ref={audioRef} loop preload="auto">
+        <source src="/assets/bgm.MP3" type="audio/mpeg" />
+      </audio>
       {isClicked ? <Rvsp/> : <Initial setIsClicked={setIsClicked}/>}
     </div>
   )
